90469eca2d6cd525ffc4a607f5fa6571efb7e57e
colinmarchbanks <marchbanks.colin@gmail.com>
2019-10-09 14:00:37 -0500

    Finished conditionals lecture exercises

--- js/functions.js
+++ js/functions.js
@@ -60,12 +60,7 @@ var random = Math.floor((Math.random() * 3) + 1);
  */
 
 function isTwo(num){
-    if(num === 2){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return num === 2;
 }
 
 console.log(isTwo(random));
