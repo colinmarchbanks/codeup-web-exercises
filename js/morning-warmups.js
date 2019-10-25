0e16c64546551d8ccadd1f29c2d960fb3917c062
colinmarchbanks <marchbanks.colin@gmail.com>
2019-10-25 09:51:01 -0500

    daily warmup

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -136,6 +136,24 @@ function createPersonObject(name,age){
     return personObject;
 }
 
+function fizzBuzz(){
+    for(let i=1;i<=100;i++){
+        if(i % 15 ===0){console.log("FizzBuzz");}
+        else if(i%5===0){console.log("Buzz");}
+        else if(i%3===0){console.log("Fizz");}
+        else{console.log(i);}
+    }
+}
+
+function returnNum5fromString(stringfive){
+    if(stringfive === "five"){
+        return 5;
+    }
+    else{
+        return "This is not the string of \'five\'";
+    }
+}
+
 const name = "Colin";
 
 // console.log(returnHi());
@@ -164,6 +182,14 @@ const name = "Colin";
 //     console.log(lengthOver8("abcdefgh"));
 //     console.log(lengthOver8("abcdefghi"));
 //     console.log(lengthOver8("abcdefg"));
-    console.log(countUpTo(50));
-    console.log(createPersonObject("Colin,24"));
+//     console.log(countUpTo(50));
+//     console.log(createPersonObject("Colin,24"));
+//     fizzBuzz();
+//     console.log(returnNum5fromString("five"));
+//     console.log(returnNum5fromString(5));
+    console.log(10 % 3);
+    var hidden = 50;
 })();
+var array = ['John', 'Paul', 'George', 'Ringo', 'Santana'];
+
+var shown = 50;
