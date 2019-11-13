755d3d43453947aac8f00e7eb1df7491ad05ed46
colinmarchbanks <marchbanks.colin@gmail.com>
2019-11-13 16:53:26 -0600

    Testing git ignore

--- js/keys.js
+++ js/keys.js
@@ -1,3 +1,3 @@
 var mapboxToken = 'pk.eyJ1IjoiY29saW5tYXJjaCIsImEiOiJjazF1dTQ4OTgwNXlwM29zN24xM2Rib3ZiIn0.X4PmRFbW0Wpddh_tO6nRyg';
 var darkskyToken = '3906045cacbf49020bba5e916ac2f5b0';
-var githubToken = 'adfce1ba45e96755569089ec09b0302e698a4095';
+var githubToken = '84c4184328b4f47252443f40d68d218eec5e5a57';
